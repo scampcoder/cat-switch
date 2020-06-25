99d13067d504d3511fab7460ebd2a454c7175e9b
Victoria Kee <victoriakee95@gmail.com>
2020-06-25 15:04:27 -0500

    Fix JS and img size

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const cat = document.getElementById('cat');
 const paw = document.getElementById('paw');
-const h1 = document.getElementById('h1');
+const h = document.getElementById('h');
 
-function switch() {
+function switchCat() {
   if(cat.src.match("white")){
-    cat.src = "img/blackcat.jpg"
-    paw.src = "img/blackpaw.jpg"
-    document.style.backgroundColor = "white";
-    h1.style.color = "black";
-    cat.class = "blackcat";
+    cat.src = "img/blackcat.jpg";
+    paw.src = "img/blackpaw.jpg";
+    document.body.style.backgroundColor = "white";
+    h.style.color = "black";
+    cat.className = "blackcat";
   } else {
-    cat.src = "img/whitecat.jpg"
-    paw.src = "img/whitepaw.jpg"
-    document.style.backgroundColor = "black";
-    h1.style.color = "white";
-    cat.class = "whitecat";
+    cat.src = "img/whitecat.jpg";
+    paw.src = "img/whitepaw.jpg";
+    document.body.style.backgroundColor = "black";
+    h.style.color = "white";
+    cat.className = "whitecat";
   }
 }
