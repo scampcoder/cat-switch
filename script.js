ce5a65fd4c0080c3d668f13abb9625d6d0f609e6
Victoria Kee <victoriakee95@gmail.com>
2020-06-25 14:27:16 -0500

    First try on main function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const cat = document.getElementById('cat');
+const paw = document.getElementById('paw');
+const h1 = document.getElementById('h1');
+
+function switch() {
+  if(cat.src.match("white")){
+    cat.src = "img/blackcat.jpg"
+    paw.src = "img/blackpaw.jpg"
+    document.style.backgroundColor = "white";
+    h1.style.color = "black";
+    cat.class = "blackcat";
+  } else {
+    cat.src = "img/whitecat.jpg"
+    paw.src = "img/whitepaw.jpg"
+    document.style.backgroundColor = "black";
+    h1.style.color = "white";
+    cat.class = "whitecat";
+  }
+}
